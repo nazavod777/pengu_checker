d9f3e76a331d2dc3f70ff03fbc30f60b356d587e
nazavod <91217742+nazavod777@users.noreply.github.com>
2024-12-23 02:25:48 +0300

    Update checker.py

--- core/checker.py
+++ core/checker.py
@@ -1,15 +1,15 @@
 import asyncio
 
+import base58
 from curl_cffi.requests import AsyncSession, Response
 from eth_account import Account
 from eth_account.messages import encode_defunct
 from eth_account.signers.local import LocalAccount
 from loguru import logger
+from solders.keypair import Keypair
 from tenacity import retry
 
-from utils import append_file
-from utils import get_proxy
-from utils import loader
+from utils import append_file, get_proxy, loader, get_account
 
 Account.enable_unaudited_hdwallet_features()
 
@@ -20,8 +20,12 @@ def log_retry_error(retry_state):
 
 class Checker:
     def __init__(self,
-                 account: LocalAccount):
-        self.account: LocalAccount = account
+                 solana_keypair: Keypair | None = None,
+                 evm_account: LocalAccount | None = None):
+        self.solana_keypair: Keypair | None = solana_keypair
+        self.evm_account: LocalAccount | None = evm_account
+
+        self.log_address: str = str(self.solana_keypair.pubkey()) if self.solana_keypair else self.evm_account.address
 
     @retry(after=log_retry_error)
     async def _get_eligibility(self,
@@ -42,13 +46,13 @@ class Checker:
                 response_text: str = r.text
 
                 if '<title>Access denied |' in response_text:
-                    raise Exception(f'{self.account.address} | CloudFlare')
+                    raise Exception(f'{self.log_address} | CloudFlare')
 
                 return r.json()['total'], r.json()['totalUnclaimed']
 
         except Exception as error:
             raise Exception(
-                f'{self.account.address} | Unexpected Error When Getting Eligibility: {error}'
+                f'{self.log_address} | Unexpected Error When Getting Eligibility: {error}'
                 + (f', response: {response_text}' if response_text else '')
             ) from error
 
@@ -66,13 +70,13 @@ class Checker:
                 response_text: str = r.text
 
                 if '<title>Access denied |' in response_text:
-                    raise Exception(f'{self.account.address} | CloudFlare')
+                    raise Exception(f'{self.log_address} | CloudFlare')
 
                 return r.json()['message'], r.json()['signingDate']
 
         except Exception as error:
             raise Exception(
-                f'{self.account.address} | Unexpected Error When Getting Sign Text: {error}'
+                f'{self.log_address} | Unexpected Error When Getting Sign Text: {error}'
                 + (f', response: {response_text}' if response_text else '')
             ) from error
 
@@ -80,42 +84,59 @@ class Checker:
     async def _get_auth_token(self,
                               sign_hash: str,
                               sign_date: str) -> str:
+        if self.evm_account:
+            payload: dict = {
+                'signature': sign_hash,
+                'signingDate': sign_date,
+                'type': 'evm',
+                'wallet': self.log_address
+            }
+
+        else:
+            payload: dict = {
+                'signature': sign_hash,
+                'signingDate': sign_date,
+                'type': 'solana',
+                'wallet': self.log_address
+            }
+
         response_text: None = None
 
         try:
             async with AsyncSession(impersonate='chrome124') as client:
                 r: Response = await client.post(
                     url='https://api.clusters.xyz/v0.1/airdrops/pengu/auth/token?',
-                    json={
-                        'signature': sign_hash,
-                        'signingDate': sign_date,
-                        'type': 'evm',
-                        'wallet': self.account.address
-                    },
+                    json=payload,
                     proxy=get_proxy()
                 )
 
                 response_text: str = r.text
 
                 if '<title>Access denied |' in response_text:
-                    raise Exception(f'{self.account.address} | CloudFlare')
+                    raise Exception(f'{self.log_address} | CloudFlare')
 
                 if not r.json()['isValid']:
                     raise Exception(
-                        f'{self.account.address} | Wrong Response When Getting Auth Token, response: {response_text}')
+                        f'{self.log_address} | Wrong Response When Getting Auth Token, response: {response_text}')
 
                 return r.json()['token']
 
         except Exception as error:
             raise Exception(
-                f'{self.account.address} | Unexpected Error When Getting Auth Token: {error}'
+                f'{self.log_address} | Unexpected Error When Getting Auth Token: {error}'
                 + (f', response: {response_text}' if response_text else '')
             ) from error
 
     async def balance_checker(self) -> None:
         sign_text, sign_date = await self._get_sign()
-        sign_hash: str = self.account.sign_message(signable_message=encode_defunct(text=sign_text)).signature.hex()
-        sign_hash: str = sign_hash if sign_hash.startswith('0x') else f'0x{sign_hash}'
+
+        if self.evm_account:
+            sign_hash: str = self.evm_account.sign_message(
+                signable_message=encode_defunct(text=sign_text)).signature.hex()
+            sign_hash: str = sign_hash if sign_hash.startswith('0x') else f'0x{sign_hash}'
+
+        else:
+            sign_hash: str = f'0x{bytes(self.solana_keypair.sign_message(message=sign_text.encode())).hex()}'
 
         auth_token: str = await self._get_auth_token(sign_hash=sign_hash,
                                                      sign_date=sign_date)
@@ -123,44 +144,46 @@ class Checker:
         account_balance, unclaimed_balance = await self._get_eligibility(auth_token=auth_token)
 
         if unclaimed_balance <= 0:
-            logger.error(f'{self.account.address} | Not Eligible')
+            logger.error(f'{self.log_address} | Not Eligible')
             return
 
-        logger.success(f'{self.account.address} | Available: {unclaimed_balance} $PENGU | Total: {account_balance} '
+        logger.success(f'{self.log_address} | Available: {unclaimed_balance} $PENGU | Total: {account_balance} '
                        f'$PENGU')
 
         async with asyncio.Lock():
-            await append_file(
-                file_path='result/with_balances.txt',
-                file_content=f'{self.account.address} | {self.account.key.hex()} | {unclaimed_balance} $PENGU\n'
-            )
+            if self.evm_account:
+                await append_file(
+                    file_path='result/with_balances_evm.txt',
+                    file_content=f'{self.evm_account.address} | {self.evm_account.key.hex()} | {unclaimed_balance} $PENGU\n'
+                )
+
+            elif self.solana_keypair:
+                await append_file(
+                    file_path='result/with_balances_sol.txt',
+                    file_content=f'{str(self.solana_keypair.pubkey())} | '
+                                 f'{base58.b58encode(bytes(self.solana_keypair.to_bytes_array())).decode("utf-8")} | '
+                                 f'{unclaimed_balance} $PENGU\n'
+                )
 
 
 async def check_account(
         account_data: str
 ) -> None:
     async with loader.semaphore:
-        account: None = None
+        account_type, account = get_account(account_data=account_data)
 
-        try:
-            account: LocalAccount = Account.from_key(private_key=account_data)
+        if account_type == 1:
+            checker: Checker = Checker(
+                evm_account=account
+            )
 
-        except Exception:
-            pass
+        elif account_type == 2:
+            checker: Checker = Checker(
+                solana_keypair=account
+            )
 
-        if not account:
-            try:
-                account: LocalAccount = Account.from_mnemonic(mnemonic=account_data)
-
-            except Exception:
-                pass
-
-        if not account:
-            logger.error(f'{account_data} | Not Mnemonic and not PKey')
+        else:
+            logger.error(f'{account_data} | Not Valid SOL/EVM PKey / Mnemonic')
             return
-
-        checker: Checker = Checker(
-            account=account
-        )
 
         return await checker.balance_checker()
