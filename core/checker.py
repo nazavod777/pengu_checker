321f028d2cd006f9a5ec6d0e00dd1fbf425f3e5e
nazavod <91217742+nazavod777@users.noreply.github.com>
2024-12-23 01:47:10 +0300

    Update checker.py

--- core/checker.py
+++ core/checker.py
@@ -2,9 +2,10 @@ import asyncio
 
 from curl_cffi.requests import AsyncSession, Response
 from eth_account import Account
+from eth_account.messages import encode_defunct
+from eth_account.signers.local import LocalAccount
 from loguru import logger
 from tenacity import retry
-from web3.auto import w3
 
 from utils import append_file
 from utils import get_proxy
@@ -13,70 +14,126 @@ from utils import loader
 Account.enable_unaudited_hdwallet_features()
 
 
-class CloudFlareError(BaseException):
-    pass
-
-
 def log_retry_error(retry_state):
     logger.error(retry_state.outcome.exception())
 
 
 class Checker:
     def __init__(self,
-                 account_address: str,
-                 account_data: str):
-        self.account_address: str = account_address
-        self.account_data: str = account_data
+                 account: LocalAccount):
+        self.account: LocalAccount = account
 
     @retry(after=log_retry_error)
-    async def _get_balance(self) -> tuple[int, int]:
+    async def _get_eligibility(self,
+                               auth_token: str) -> tuple[int, int]:
         response_text: None = None
 
         try:
             async with AsyncSession(impersonate='chrome124') as client:
-                response_text: None = None
-
-                r: Response = await client.get(
-                    url=f'https://api.clusters.xyz/v0.1/airdrops/pengu/eligibility/{w3.to_checksum_address(value=self.account_address)}?',
+                r: Response = await client.post(
+                    url='https://api.clusters.xyz/v0.1/airdrops/pengu/eligibility',
+                    json=[auth_token],
                     proxy=get_proxy(),
                     headers={
-                        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-                        'accept-language': 'ru,en;q=0.9',
-                    },
-                    timeout=15
+                        'content-type': 'application/json'
+                    }
                 )
 
-            if '<title>Access denied |' in r.text:
-                raise Exception(f'{self.account_address} | CloudFlare')
+                response_text: str = r.text
 
-            response_text: str = r.text
-            response_json: dict = r.json()
+                if '<title>Access denied |' in response_text:
+                    raise Exception(f'{self.account.address} | CloudFlare')
 
-            return response_json['total'], response_json['totalUnclaimed']
+                return r.json()['total'], r.json()['totalUnclaimed']
 
         except Exception as error:
             raise Exception(
-                f'{self.account_address} | Unexpected Error When Checking Eligible: {error}'
+                f'{self.account.address} | Unexpected Error When Getting Eligibility: {error}'
+                + (f', response: {response_text}' if response_text else '')
+            ) from error
+
+    @retry(after=log_retry_error)
+    async def _get_sign(self) -> tuple[str, str]:
+        response_text: None = None
+
+        try:
+            async with AsyncSession(impersonate='chrome124') as client:
+                r: Response = await client.get(
+                    url='https://api.clusters.xyz/v0.1/airdrops/pengu/auth/message?',
+                    proxy=get_proxy()
+                )
+
+                response_text: str = r.text
+
+                if '<title>Access denied |' in response_text:
+                    raise Exception(f'{self.account.address} | CloudFlare')
+
+                return r.json()['message'], r.json()['signingDate']
+
+        except Exception as error:
+            raise Exception(
+                f'{self.account.address} | Unexpected Error When Getting Sign Text: {error}'
+                + (f', response: {response_text}' if response_text else '')
+            ) from error
+
+    @retry(after=log_retry_error)
+    async def _get_auth_token(self,
+                              sign_hash: str,
+                              sign_date: str) -> str:
+        response_text: None = None
+
+        try:
+            async with AsyncSession(impersonate='chrome124') as client:
+                r: Response = await client.post(
+                    url='https://api.clusters.xyz/v0.1/airdrops/pengu/auth/token?',
+                    json={
+                        'signature': sign_hash,
+                        'signingDate': sign_date,
+                        'type': 'evm',
+                        'wallet': self.account.address
+                    },
+                    proxy=get_proxy()
+                )
+
+                response_text: str = r.text
+
+                if '<title>Access denied |' in response_text:
+                    raise Exception(f'{self.account.address} | CloudFlare')
+
+                if not r.json()['isValid']:
+                    raise Exception(
+                        f'{self.account.address} | Wrong Response When Getting Auth Token, response: {response_text}')
+
+                return r.json()['token']
+
+        except Exception as error:
+            raise Exception(
+                f'{self.account.address} | Unexpected Error When Getting Auth Token: {error}'
                 + (f', response: {response_text}' if response_text else '')
             ) from error
 
     async def balance_checker(self) -> None:
-        account_balance, unclaimed_balance = await self._get_balance()
+        sign_text, sign_date = await self._get_sign()
+        sign_hash: str = self.account.sign_message(signable_message=encode_defunct(text=sign_text)).signature.hex()
+        sign_hash: str = sign_hash if sign_hash.startswith('0x') else f'0x{sign_hash}'
 
-        if account_balance <= 0:
-            logger.error(f'{self.account_address} | Not Eligible')
+        auth_token: str = await self._get_auth_token(sign_hash=sign_hash,
+                                                     sign_date=sign_date)
+
+        account_balance, unclaimed_balance = await self._get_eligibility(auth_token=auth_token)
+        available_balance: int = account_balance - unclaimed_balance
+
+        if available_balance <= 0:
+            logger.error(f'{self.account.address} | Not Eligible')
             return
 
-        if unclaimed_balance <= 0:
-            logger.info(f'{self.account_address} | Claimed')
-            return
-
-        logger.success(f'{self.account_address} | {self.account_data} | {account_balance} $PENGU')
+        logger.success(f'{self.account.address} | Available: {available_balance} $PENGU | Total: {account_balance} '
+                       f'$PENGU | Unclaimed: {unclaimed_balance} $PENGU')
 
         async with asyncio.Lock():
             await append_file(
                 file_path='result/with_balances.txt',
-                file_content=f'{self.account_address} | {self.account_data} | {account_balance} $PENGU\n'
+                file_content=f'{self.account.address} | {self.account.key.hex()} | {available_balance} $PENGU\n'
             )
 
 
@@ -84,35 +141,27 @@ async def check_account(
         account_data: str
 ) -> None:
     async with loader.semaphore:
-        account_address: None = None
+        account: None = None
 
         try:
-            account_address: str = Account.from_key(private_key=account_data).address
+            account: LocalAccount = Account.from_key(private_key=account_data)
 
         except Exception:
             pass
 
-        if not account_address:
+        if not account:
             try:
-                account_address: str = Account.from_mnemonic(mnemonic=account_data).address
+                account: LocalAccount = Account.from_mnemonic(mnemonic=account_data)
 
             except Exception:
                 pass
 
-        if not account_address:
-            try:
-                account_address: str = w3.to_checksum_address(value=account_data)
-
-            except Exception:
-                pass
-
-        if not account_address:
+        if not account:
             logger.error(f'{account_data} | Not Mnemonic and not PKey')
             return
 
         checker: Checker = Checker(
-            account_address=account_address,
-            account_data=account_data
+            account=account
         )
 
         return await checker.balance_checker()
